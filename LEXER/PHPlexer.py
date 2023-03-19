fca8965f8219d553b2237478538c955fbf1780dc
Migueldylo <flepard3612@gmail.com>
2023-03-19 17:43:12 -0500

    setting up

---
+++ LEXER/PHPlexer.py
@@ -0,0 +1,217 @@
+# ------------------------------------------------------------
+# PHPlexer.py
+#
+# tokenizer for a PHP-like syntax
+# This module just contains the lexing rules
+# ------------------------------------------------------------
+
+#Token lists
+
+reserved = {
+#Keywords
+    "ABSTRACT": "ABSTRACT",
+    "BREAK": "BREAK",
+    "ARRAY": "ARRAY",
+    "CALLABLE": "CALLABLE",
+    "CASE": "CASE",
+    "CATCH": "CATCH",
+    "CLASS": "CLASS",
+    "CLONE": "CLONE",
+    "CONST": "CONST",
+    "CONTINUE": "CONTINUE",
+    "DECLARE": "DECLARE",
+    "DEFAULT": "DEFAULT",
+    "DIE": "DIE",
+    "DO": "DO",
+    "ECHO": "ECHO",
+    "ELSE": "ELSE",
+    "ELSEIF": "ELSEIF",
+    "EMPTY": "EMPTY",
+    "ENDDECLARE": "ENDDECLARE",
+    "ENDFOR": "ENDFOR",
+    "ENDFOREACH": "ENDFOREACH",
+    "ENDIF": "ENDIF",
+    "ENDSWITCH": "ENDSWITCH",
+    "ENDWHILE": "ENDWHILE",
+    "EVAL": "EVAL",
+    "EXIT": "EXIT",
+    "EXTENDS": "EXTENDS",
+    "FINAL": "FINAL",
+    "FINALLY": "FINALLY",
+    "FN": "FN",
+    "FOR": "FOR",
+    "FOREACH": "FOREACH",
+    "FUNCTION": "FUNCTION",
+    "GLOBAL": "GLOBAL",
+    "GOTO": "GOTO",
+    "IF": "IF",
+    "IMPLEMENTS": "IMPLEMENTS",
+    "INCLUDE": "INCLUDE",
+    "INCLUDE_ONCE": "INCLUDE_ONCE",
+    "INSTANCEOF": "INSTANCEOF",
+    "INSTEADOF": "INSTEADOF",
+    "INTERFACE": "INTERFACE",
+    "ISSET": "ISSET",
+    "LIST": "LIST",
+    "MATCH": "MATCH",
+    "NAMESPACE": "NAMESPACE",
+    "NEW": "NEW",
+    "OR": "OR",
+    "PRINT": "PRINT",
+    "PRIVATE": "PRIVATE",
+    "PROTECTED": "PROTECTED",
+    "PUBLIC": "PUBLIC",
+    "READONLY": "READONLY",
+    "REQUIRE": "REQUIRE",
+    "REQUIRE_ONCE": "REQUIRE_ONCE",
+    "RETURN": "RETURN",
+    "STATIC": "STATIC",
+    "SWITCH": "SWITCH",
+    "THROW": "THROW",
+    "TRAIT": "TRAIT",
+    "TRY": "TRY",
+    "UNSET": "UNSET",
+    "USE": "USE",
+    "VAR": "VAR",
+    "WHILE": "WHILE",
+    "XOR": "XOR",
+    "YIELD": "YIELD",
+    "INT": "INT",
+    "FLOAT": "FLOAT",
+    "BOOL": "BOOL",
+    "STRING": "STRING",
+    "TRUE": "TRUE",
+    "FALSE": "FALSE",
+    "NULL": "NULL",
+    "VOID": "VOID",
+    "ITERABLE": "ITERABLE",
+    "OBJECT": "OBJECT",
+    "MIXED": "MIXED",
+    "NEVER": "NEVER",
+}
+tokens = [
+    # Symbols
+    'PLUS',
+    'PLUSPLUS',
+    'PLUSEQUAL',
+    'MINUS',
+    'MINUSMINUS',
+    'MINUSEQUAL',
+    'TIMES',
+    'DIVIDE',
+    'LESS',
+    'LESSEQUAL',
+    'GREATER',
+    'GREATEREQUAL',
+    'EQUAL',
+    'DEQUAL',
+    'DISTINT',
+    'ISEQUAL',
+    'SEMICOLON',
+    'COMMA',
+    'LPAREN',
+    'RPAREN',
+    'LBRACKET',
+    'RBRACKET',
+    'LBLOCK',
+    'RBLOCK',
+    'COLON',
+    'AMPERSANT',
+    'HASHTAG',
+    'DOT',
+    'QUOTE',
+    'DOUBLEQUOTE',
+    'DOLLARSIGN',
+    'QUESTIONMARK',
+    #Others
+    "ID",
+    "NUMBER"
+] + list(reserved.values())
+
+#Token definition
+
+t_PLUS   = r'\+'
+t_MINUS  = r'-'
+t_TIMES  = r'\*'
+t_DIVIDE = r'/'
+t_LESS = r'<'
+t_GREATER = r'>'
+t_EQUAL  = r'='
+t_DISTINT = r'!'
+t_SEMICOLON = ';'
+t_COMMA  = r','
+t_LPAREN = r'\('
+t_RPAREN  = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBLOCK   = r'{'
+t_RBLOCK   = r'}'
+t_COLON   = r':'
+t_AMPERSANT = r'\&'
+t_HASHTAG = r'\#'
+t_DOT = r'\.'
+t_QUOTE = r'\''
+t_DOUBLEQUOTE = r'\"'
+t_DOLLARSIGN = r'\$'
+t_QUESTIONMARK = r'\?'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value.upper(),'ID')
+    return t
+
+def t_NUMBER(t):
+    r'\d+(\.\d+)?((E|e)(-|\+)?\d+(\.\d+)?)?'  # possible
+    t.value = float(t.value)
+    return t
+
+def t_LESSEQUAL(t):
+    r'<='
+    return t
+
+
+def t_GREATEREQUAL(t):
+    r'>='
+    return t
+
+
+def t_DEQUAL(t):
+    r'!='
+    return t
+
+
+def t_ISEQUAL(t):
+    r'=='
+    return t
+
+
+def t_MINUSMINUS(t):
+    r'--'
+    return t
+
+
+def t_PLUSPLUS(t):
+    r'\+\+'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t'
+
+def t_comments(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+
+def t_comments_C99(t):
+    r'//(.)*?\n'
+    t.lexer.lineno += 1
+
+def t_error(t):
+    print("Lexical error: " + str(t.value[0]))
+    t.lexer.skip(1)
+
+
+
---
+++ LEXER/main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press ⌥⇧R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+import PHPlexer
+import ply.lex as lexer
+
+
+def test(data, builtLexer):
+    builtLexer.input(data)
+    while True:
+        tok = builtLexer.token()
+        if not tok:
+            break
+        print(tok)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Reading arguments from command line
+    # if we pass more than one argument, the only string that will be caught is test.txt
+    # Example of what you don't have to pass: python3 test.txt <filename> ...
+
+    if len(sys.argv) > 1:
+        fin = sys.argv[1]
+    else:
+        fin = 'test.txt'
+    f = open(fin, 'r')
+    data = f.read()
+    print(data)
+    #Build the lexer
+    miniPHP = lexer.lex(module=PHPlexer)
+    test(data, miniPHP)
+    # input()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
