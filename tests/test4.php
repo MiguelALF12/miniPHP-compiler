d111174b53d9da16b2165335b1fe00f3e47e6f53
Migueldylo <flepard3612@gmail.com>
2023-06-08 14:15:39 -0500

    Addingn the last reviewed version of the compiler, there I only got to the parser and was pending to add the error on specific place recognition

---
+++ tests/test4.php
@@ -0,0 +1,15 @@
+<?php
+//Original Array on which operations is to be perform
+
+$original_array = 0;
+$someString = "hello";
+echo $original_array
+
+function fibo($arr){
+for($hola=1;$hola > 0; $i++){
+    echo $i;
+}
+return $arr
+}
+
+?>
